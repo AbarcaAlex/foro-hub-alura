98ae74b2945943c2738b25f1e7a19f01ef1f3d28
Alexis Abarca <alexisabarcaz@outlook.com>
2024-06-27 15:34:26 -0600

    Revert "se agrego la conexion a la base de datos"
    This reverts commit 25b677c19ffc8c3858af8831afe5e22834d2f655.

--- src/main/resources/db/migration/V1__create-table-usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuario(
-    id BIGINT generated always as identity not null primary key,
-    nombre varchar(200) not null,
-    correo varchar(200) not null,
-    clave varchar(255) not null
-);
